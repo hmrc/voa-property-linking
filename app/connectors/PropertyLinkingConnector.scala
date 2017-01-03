09ed6265306ee2d87af736ac2fa9e024aa23a812
richard.dennehy <richard.dennehy@digital.voa.gov.uk>
2017-01-03 11:26:43 +0000

    update copyright notice

--- app/connectors/PropertyLinkingConnector.scala
+++ app/connectors/PropertyLinkingConnector.scala
@@ -1,5 +1,5 @@
 /*
- * Copyright 2016 HM Revenue & Customs
+ * Copyright 2017 HM Revenue & Customs
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -45,4 +45,3 @@ class PropertyLinkingConnector(http: HttpGet with HttpPut with HttpPost)(implici
     http.GET[Option[PropertyLink]](url)
   }
 }
-
