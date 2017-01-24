4c47c330da19066731641bb599ff9084e5c578c0
Christophe Dupre <ch_dupre@yahoo.com>
2017-01-24 16:03:56 +0000

    headers for ApiManager

--- app/config/Wiring.scala
+++ app/config/Wiring.scala
@@ -17,15 +17,16 @@
 package config
 
 import connectors._
+import play.api.libs.json.Writes
 import uk.gov.hmrc.play.audit.http.config.LoadAuditingConfig
 import uk.gov.hmrc.play.audit.http.connector.AuditConnector
 import uk.gov.hmrc.play.auth.microservice.connectors.AuthConnector
 import uk.gov.hmrc.play.config.{AppName, RunMode, ServicesConfig}
 import uk.gov.hmrc.play.http.hooks.HttpHook
 import uk.gov.hmrc.play.http.ws._
-import uk.gov.hmrc.play.http.{HttpDelete, HttpGet, HttpPost, HttpPut}
+import uk.gov.hmrc.play.http._
 
-import scala.concurrent.ExecutionContext
+import scala.concurrent.{ExecutionContext, Future}
 
 object Wiring {
   def apply() = play.api.Play.current.global.asInstanceOf[MicroserviceGlobal].wiring
@@ -33,17 +34,57 @@ object Wiring {
 
 abstract class Wiring {
   val http: HttpGet with HttpPut with HttpDelete with HttpPost
+  val backendHttp: HttpGet with HttpPut with HttpDelete with HttpPost
 
   implicit lazy val ec: ExecutionContext = play.api.libs.concurrent.Execution.Implicits.defaultContext
-  lazy val propertyRepresentationConnector = new PropertyRepresentationConnector(http)
-  lazy val propertyLinkingConnector = new PropertyLinkingConnector(http)
-  lazy val individualAccounts = new IndividualAccountConnector(http)
-  lazy val groupAccounts = new GroupAccountConnector(http)
-  lazy val addresses = new AddressConnector(http)
+  lazy val propertyRepresentationConnector = new PropertyRepresentationConnector(backendHttp)
+  lazy val propertyLinkingConnector = new PropertyLinkingConnector(backendHttp)
+  lazy val individualAccounts = new IndividualAccountConnector(backendHttp)
+  lazy val groupAccounts = new GroupAccountConnector(backendHttp)
+  lazy val addresses = new AddressConnector(backendHttp)
 }
 
 object WSHttp extends WSGet with WSPut with WSPost with WSDelete with WSPatch with AppName {
   override val hooks: Seq[HttpHook] = NoneRequired
+}
+
+object VOABackendWSHttp extends WSHttp with ServicesConfig {
+  override val hooks: Seq[HttpHook] = NoneRequired
+  override def doGet(url: String)(implicit hc: HeaderCarrier): Future[HttpResponse] = {
+    super.doGet(url)(hc.withExtraHeaders(
+        ("Ocp-Apim-Subscription-Key" , ApplicationConfig.apiConfigSubscriptionKeyHeader),
+        ("Ocp-Apim-Trace" , ApplicationConfig.apiConfigTraceHeader)
+    ))
+  }
+
+  override def doDelete(url: String)(implicit hc: HeaderCarrier): Future[HttpResponse] = {
+    super.doDelete(url)(hc.withExtraHeaders(
+      ("Ocp-Apim-Subscription-Key" , ApplicationConfig.apiConfigSubscriptionKeyHeader),
+      ("Ocp-Apim-Trace" , ApplicationConfig.apiConfigTraceHeader)
+    ))
+  }
+
+  override def doPatch[A](url: String, body: A)(implicit rds: Writes[A], hc: HeaderCarrier): Future[HttpResponse] = {
+    super.doPatch(url, body)(rds, hc.withExtraHeaders(
+      ("Ocp-Apim-Subscription-Key" , ApplicationConfig.apiConfigSubscriptionKeyHeader),
+      ("Ocp-Apim-Trace" , ApplicationConfig.apiConfigTraceHeader)
+    ))
+  }
+
+  override def doPut[A](url: String, body: A)(implicit rds: Writes[A], hc: HeaderCarrier): Future[HttpResponse] = {
+    super.doPut(url, body)(rds, hc.withExtraHeaders(
+      ("Ocp-Apim-Subscription-Key" , ApplicationConfig.apiConfigSubscriptionKeyHeader),
+      ("Ocp-Apim-Trace" , ApplicationConfig.apiConfigTraceHeader)
+    ))
+  }
+
+  override def doPost[A](url: String, body: A, headers: Seq[(String, String)])(implicit rds: Writes[A], hc: HeaderCarrier): Future[HttpResponse] = {
+    super.doPost(url, body, headers)(rds, hc.withExtraHeaders(
+      ("Ocp-Apim-Subscription-Key" , ApplicationConfig.apiConfigSubscriptionKeyHeader),
+      ("Ocp-Apim-Trace" , ApplicationConfig.apiConfigTraceHeader)
+    ))
+  }
+
 }
 
 object MicroserviceAuditConnector extends AuditConnector with RunMode {
