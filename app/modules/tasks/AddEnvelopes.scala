8a9b3e4cacffe1662f6d1efdad7f0428e7f6ca4d
Andrew Walters <andrew.walters@digital.voa.gov.uk>
2017-07-03 11:32:55 +0100

    Adding additional envelopes

--- app/modules/tasks/AddEnvelopes.scala
+++ app/modules/tasks/AddEnvelopes.scala
@@ -30,7 +30,7 @@ import scala.util.Try
 
 @Singleton
 class AddEnvelopes @Inject()(override val env: Environment, envelopeRepo: EnvelopeIdRepository) extends MongoTask[EnvelopeId] {
-  override val upToVersion: Int = 3
+  override val upToVersion: Int = 4
   override def verify: String => Option[EnvelopeId] = line => Try(UUID.fromString(line)).map(_ => Some(EnvelopeId(line, line, Some(Closed)))).getOrElse(None)
   override def execute: EnvelopeId => Future[Unit] = line => envelopeRepo.insert(line)
     .map(_ => Logger.info(s"Added: $line"))
