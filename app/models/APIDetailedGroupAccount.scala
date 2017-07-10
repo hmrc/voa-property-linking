bf22f1c6b920cad420d0e72345db22270510299c
richard.dennehy <richard.dennehy@digital.voa.gov.uk>
2017-07-10 10:15:39 +0100

    ignore small business flag

--- app/models/APIDetailedGroupAccount.scala
+++ app/models/APIDetailedGroupAccount.scala
@@ -25,14 +25,14 @@ case class APIDetailedGroupAccount(id: Int, governmentGatewayGroupId: String, re
   def toGroupAccount = {
     GroupAccount(id, governmentGatewayGroupId, organisationLatestDetail.organisationName, organisationLatestDetail.addressUnitId,
       organisationLatestDetail.organisationEmailAddress, organisationLatestDetail.organisationTelephoneNumber.getOrElse("not set"),
-      organisationLatestDetail.smallBusinessFlag, organisationLatestDetail.representativeFlag, representativeCode)
+      organisationLatestDetail.representativeFlag, representativeCode)
   }
 }
 
 case class IndividualSummary(personLatestDetail: APIIndividualDetails)
 
-case class GroupDetails(addressUnitId: Int, representativeFlag: Boolean, smallBusinessFlag: Boolean, organisationName: String,
-                        organisationEmailAddress: String, organisationTelephoneNumber: Option[String], effectiveFrom: LocalDate)
+case class GroupDetails(addressUnitId: Int, representativeFlag: Boolean, organisationName: String, organisationEmailAddress: String,
+                        organisationTelephoneNumber: Option[String], effectiveFrom: LocalDate)
 
 object GroupDetails {
   implicit val format = Json.format[GroupDetails]
