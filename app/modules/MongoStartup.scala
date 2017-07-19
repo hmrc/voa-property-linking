0a401755c005b16121ec7053649e10101910eb62
Richard Dennehy <richard.dennehy@outlook.com>
2017-07-19 11:09:02 +0100

    VTCCA-778 add ttl and timestamp to envelope records (#104)

--- app/modules/MongoStartup.scala
+++ app/modules/MongoStartup.scala
@@ -20,7 +20,7 @@ import java.time.LocalDateTime
 
 import com.google.inject.multibindings.Multibinder
 import com.google.inject.{AbstractModule, Inject, Singleton, TypeLiteral}
-import modules.tasks.{AddEnvelopes, RemoveEnvelopes}
+import modules.tasks.{AddEnvelopes, AddTimestamps, RemoveEnvelopes}
 import org.joda.time.Duration
 import play.api.{Configuration, Environment, Logger}
 import play.modules.reactivemongo.ReactiveMongoComponent
@@ -36,8 +36,7 @@ import scala.io.Source
 class MongoStartup(environment: Environment, configuration: Configuration) extends AbstractModule {
   def configure(): Unit = {
     val mb = Multibinder.newSetBinder(binder(), new TypeLiteral[MongoTask[_]]() {})
-    mb.addBinding().to(classOf[RemoveEnvelopes])
-    mb.addBinding().to(classOf[AddEnvelopes])
+    mb.addBinding().to(classOf[AddTimestamps])
 
     bind(classOf[MongoStartupRunner]).to(classOf[MongoStartupRunnerImpl]).asEagerSingleton()
   }
