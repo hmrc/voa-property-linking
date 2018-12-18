4a197c257a7385633b8d27d26dc2c1a2d7b8ae36
Habeeb <habeeb.olayemi@digital.hmrc.gov.uk>
2018-12-18 14:04:21 +0000

    VTCCA-2395: changed name of assessmentRef field

--- app/models/casesModel.scala
+++ app/models/casesModel.scala
@@ -103,7 +103,7 @@ case class OwnerCheckCase(checkCaseSubmissionId: String,
                           settledDate: Option[LocalDate],
                           agent: Option[Agent],
                           submittedBy: String,
-                          assessmentRef: Option[Long] = None)
+                          originatingAssessmentReference: Option[Long] = None)
                           extends CheckCase
 
 object OwnerCheckCase {
@@ -120,7 +120,7 @@ case class AgentCheckCase(checkCaseSubmissionId: String,
                           settledDate: Option[LocalDate],
                           client: Client,
                           submittedBy: String,
-                          assessmentRef: Option[Long] = None)
+                          originatingAssessmentReference: Option[Long] = None)
                           extends CheckCase
 
 object AgentCheckCase {
