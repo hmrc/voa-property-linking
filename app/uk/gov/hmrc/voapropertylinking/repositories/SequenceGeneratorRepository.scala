26b2ab5a8fcb70d37e2f81cc4468afdba664b0e5
Reddy Yattapu <tripindicator@gmail.com>
2020-02-28 10:17:11 +0000

    Ccaia 877 (#259)
    * CCAIA-877 Preserve MDC context in Session repository
    
    * CCAIA-877 Preserve MDC context in Session repository

--- app/uk/gov/hmrc/voapropertylinking/repositories/SequenceGeneratorRepository.scala
+++ app/uk/gov/hmrc/voapropertylinking/repositories/SequenceGeneratorRepository.scala
@@ -18,12 +18,14 @@ package uk.gov.hmrc.voapropertylinking.repositories
 
 import com.google.inject.Singleton
 import javax.inject.Inject
+
 import play.api.libs.json._
 import play.modules.reactivemongo.ReactiveMongoComponent
 import reactivemongo.bson.{BSONDocument, BSONInteger, BSONString}
 import reactivemongo.play.json.ImplicitBSONHandlers._
 import reactivemongo.play.json.collection.JSONBatchCommands.FindAndModifyCommand
 import uk.gov.hmrc.mongo.ReactiveRepository
+import uk.gov.hmrc.play.http.logging.Mdc
 
 import scala.concurrent.ExecutionContext.Implicits.global
 import scala.concurrent.Future
@@ -56,15 +58,16 @@ class SequenceGeneratorMongoRepository @Inject()(mongo: ReactiveMongoComponent)
     val selector: BSONDocument = BSONDocument("_id" -> BSONString(key))
 
     def update(inc: Int): BSONDocument = BSONDocument(s"$$inc" -> BSONDocument("sequence" -> BSONInteger(inc)))
-
-    findLatestSequence(key).flatMap { optSequence =>
-      optSequence.fold(
-        collection.findAndUpdate(selector, update(600000000), fetchNewObject = true, upsert = true).map(sequenceNo)
-      ) {
-        case sequence if sequence.sequence == 999999999 =>
-          Future.failed(new RuntimeException("Reached upper limit of 999999999 on generating sequence number"))
-        case _ =>
-          collection.findAndUpdate(selector, update(1), fetchNewObject = true, upsert = false).map(sequenceNo)
+    Mdc.preservingMdc {
+      findLatestSequence(key).flatMap { optSequence =>
+        optSequence.fold(
+          collection.findAndUpdate(selector, update(600000000), fetchNewObject = true, upsert = true).map(sequenceNo)
+        ) {
+          case sequence if sequence.sequence == 999999999 =>
+            Future.failed(new RuntimeException("Reached upper limit of 999999999 on generating sequence number"))
+          case _ =>
+            collection.findAndUpdate(selector, update(1), fetchNewObject = true, upsert = false).map(sequenceNo)
+        }
       }
     }
   }
