e20a895766dd929539a73db79cb6d936c28b09f6
MarkAKelly <21219364+MarkAKelly@users.noreply.github.com>
2023-06-20 19:07:27 +0100

    VTCCA-5795 add listYears to get agent

--- app/models/modernised/externalpropertylink/myorganisations/AgentList.scala
+++ app/models/modernised/externalpropertylink/myorganisations/AgentList.scala
@@ -25,7 +25,8 @@ case class AgentSummary(
       representativeCode: Long,
       name: String,
       appointedDate: LocalDate,
-      propertyCount: Int
+      propertyCount: Int,
+      listYears: Option[List[String]] = None
 )
 
 object AgentSummary {
